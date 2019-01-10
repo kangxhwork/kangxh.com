e78e4d5c88ac0374ab4a8f103e9b434cba059624
Allen Kang <allenk@microsoft.com>
2019-01-10 10:12:22 +0800

    1-10 containers

---
+++ kangxh.com/kangxh.com/container/managek8scluster.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="managek8scluster.aspx.cs" Inherits="kangxh.com.container.managek8scluster" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="startK8sCluster" runat="server" OnClick="startK8sCluster_Click" Text="Start K8S Cluster" />
+            <asp:Button ID="stopK8sCluster" runat="server" Text="Stop K8S Cluster" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ kangxh.com/kangxh.com/container/managek8scluster.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+
+namespace kangxh.com.container
+{
+    public partial class managek8scluster : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void startK8sCluster_Click(object sender, EventArgs e)
+        {
+            string webhookURL = (WebConfigurationManager.ConnectionStrings["StartVMWebhookURL"]).ConnectionString;
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                string postResult = webClient.UploadString(webhookURL, "");
+                Response.Write(postResult);
+            }
+        }
+    }
+}
